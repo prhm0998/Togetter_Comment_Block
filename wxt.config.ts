6133e5a88aee21bd14350322fcdb1f52177d7aa0
prhm0998 <myyah0998@hotmail.co.jp>
2025-10-11 14:57:22 +0900

    change-github-actions @wxt-i18nを追加 アプリ名をlocaleからの読み込みに変更

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-vue', '@wxt-dev/auto-icons'],
+  modules: ['@wxt-dev/i18n/module', '@wxt-dev/module-vue', '@wxt-dev/auto-icons'],
   vite: () => {
     const isProd = process.env.NODE_ENV === 'production'
     return {
@@ -19,7 +19,8 @@ export default defineConfig({
   },
   manifest: () => ({
     permissions: ['storage'],
-    name: process.env.NODE_ENV === 'production' ? import.meta.env.WXT_EXT_NAME : `(Dev)${import.meta.env.WXT_EXT_NAME}`,
-    description: import.meta.env.WXT_EXT_DESC,
+    name: '__MSG_name__',
+    description: '__MSG_description__',
+    default_locale: 'ja',
   }),
 })
